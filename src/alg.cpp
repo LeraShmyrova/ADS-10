70d41c1faf1c075ccbd325ccb7172609f28662be
LeraShmyrova <94710005+LeraShmyrova@users.noreply.github.com>
2022-06-05 21:46:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,11 +5,11 @@
 #include  <cstdlib>
 #include  "tree.h"
 
-using namespace std;
+using std::vector;
 vector<char> getPerm(const Tree& tr, int k) {
   vector<char> resultat;
   for (int j = 0; j < tr[k - 1].length(); j++) {
   resultat.push_back(tr[k - 1][j]);
-    }  
-    return resultat;
+  }
+  return resultat;
 }
